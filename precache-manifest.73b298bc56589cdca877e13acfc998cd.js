f5db7a842c92fcb7b136abf24549df74a0a5dbf0
fagalovar <fagalovar@gmail.com>
2020-05-07 21:20:41 +0500

    Updates

---
+++ precache-manifest.73b298bc56589cdca877e13acfc998cd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c17fe30c2b692c30eedddd4ca2f743f4",
+    "url": "/VMeste/index.html"
+  },
+  {
+    "revision": "014814202266aebf6be6",
+    "url": "/VMeste/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "d4eeb08db0c8d9755716",
+    "url": "/VMeste/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "014814202266aebf6be6",
+    "url": "/VMeste/static/js/2.fe65863a.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/VMeste/static/js/2.fe65863a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4eeb08db0c8d9755716",
+    "url": "/VMeste/static/js/main.5c288842.chunk.js"
+  },
+  {
+    "revision": "f37565cc5b1838662de9",
+    "url": "/VMeste/static/js/runtime-main.7b170afb.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/VMeste/static/media/persik.4e1ec840.png"
+  }
+]);
